09578a405c02b53a442fc25983bd224d83acf4b0
Luiz Filipe Port <luizfilipeport@gmail.com>
2024-12-31 19:56:29 -0300

    last commit...

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -30,22 +30,22 @@ const CookiePopup: React.FC = () => {
   if (!showPopup) return null;
 
   return (
-    <div className="fixed bottom-0 lg:right-0 lg:left-[47px] flex bg-opacity-90 z-50 sm: mx-[30px]">
+    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 lg:right-0 lg:left-[47px] flex bg-opacity-90 z-50 sm:w-auto">
       <div className="bg-white p-6 lg:w-[542px] lg:h-[245px] sm: w-[348px] sm: h-[158px] lg:px-[51px] border shadow-sm">
-        <p className="text-center text-black lg:text-[18px] lg:leading-[29px] sm: text-[11px] sm: leading-[19px] lg:mb-8 sm: mb-4 lg:pt-[38px]">
+        <p className="text-center text-black lg:text-[18px] lg:leading-[29px] sm: text-[11px] sm: leading-[19px] lg:mb-8 sm: mb-4 lg:pt-[38px] font-redhat">
           Este site utiliza cookies para melhorar sua experiência e para
           analisar o desempenho de tráfego.
         </p>
         <div className="flex flex-row justify-center items-center lg:mb-[47px] lg:text-[17px] lg:leading-[21px] sm: text-[11px] sm: leading-[13px]">
           <button
             onClick={handleAccept}
-            className="lg:w-[219px] lg:h-[64px] sm: w-[145px] sm: h-[41px] bg-[#5C7383] text-white px-6 py-2 hover:scale-110 transition-transform duration-300"
+            className="lg:w-[219px] lg:h-[64px] sm: w-[145px] sm: h-[41px] bg-[#5C7383] text-white px-6 py-2 hover:scale-110 transition-transform duration-300 font-redhat"
           >
             Permitir cookies
           </button>
           <button
             onClick={handleDismiss}
-            className="lg:w-[103px] lg:h-[64px] sm: w-[60px] sm: h-[41px] bg-white text-[#5C7383] hover:text-gray-700 hover:scale-110 transition-transform duration-300"
+            className="lg:w-[103px] lg:h-[64px] sm: w-[60px] sm: h-[41px] bg-white text-[#5C7383] hover:text-gray-700 hover:scale-110 transition-transform duration-300 font-redhat"
           >
             Dispensar
           </button>
